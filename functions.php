b52e87a24df57690ef74369eb6a83946bc6f9fb0
ttijs <59823888+ttijs@users.noreply.github.com>
2021-11-03 10:39:05 +0100

    eerste stap van  sidevbar

--- functions.php
+++ functions.php
@@ -5,4 +5,17 @@ function mijnthema_menu() {
 }
 add_action('init', 'mijnthema_menu');
 
+
+function arphabet_widgets_init() {
+	register_sidebar( array(
+		'name'          => 'Sidebar',
+		'id'            => 'sidebar-widgets',
+		'before_widget' => '<div class="side-widget">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="rounded">',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
 ?>
