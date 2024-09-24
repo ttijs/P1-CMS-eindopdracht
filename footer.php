4ebe00b5e0c862dc509e70211861bef0707972b9
ttijs <ttijs@glu.nl>
2024-09-24 22:01:07 +0200

    footer widget opmaak aangepast

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
 	<footer>
 
-	<?php dynamic_sidebar( 'mijn-footer-widgets' ); ?>
 
 
 
 		<section class="container footer">
+
+			<?php dynamic_sidebar('mijn-footer-widgets'); ?>
+
 			<div class="row no-gutters">
 				<img src="<?php bloginfo('template_url'); ?>/imgs/guy-watching-over-valley2.jpg" class='img-fluid img-fit' alt='Guy watching over valley'>
 			</div>
@@ -37,14 +39,14 @@
 			</div>
 		</section>
 	</footer>
-	
-<!--jQuery & Bootstrap JS-->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
-<!--Custom JavaScript-->
-<script src="js/main.js"></script>	
-	
-</body>
-</html>
+	<!--jQuery & Bootstrap JS-->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
+	<!--Custom JavaScript-->
+	<script src="js/main.js"></script>
+
+	</body>
+
+	</html>
